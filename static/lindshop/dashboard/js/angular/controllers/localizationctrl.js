4b995fa39cfd6e2455aeab7fb9a7438ebd8d0166
marcuslind90 <marcuslind90@gmail.com>
2016-07-13 17:02:38 +0700

    Added Country, Tax and Currency Settings on Dashboard

---
+++ static/lindshop/dashboard/js/angular/controllers/localizationctrl.js
@@ -0,0 +1,84 @@
+'use strict';
+angular.module('dashboard')
+.controller('localizationCtrl', function($scope, $http, $location){
+	// Set config for HTTP calls
+	var config = {
+		params: {
+			callback: 'JSON_CALLBACK', 
+		}, 
+		cache: true, 
+	}
+
+	getCountries(config, function(response) {
+		$scope.countries = response.data;
+	});
+
+
+	function getCountries(config, callback) {
+		$http.get('/api/countries/', config).then(function(response) {
+			callback(response);
+		});
+	}
+})
+.controller('countryCtrl', function($scope, $http, $location, $routeParams){
+	var config = {
+		params: {
+			callback: 'JSON_CALLBACK', 
+		}, 
+		cache: true, 
+	}
+
+	getCountry(config, function(response){
+		$scope.country = response.data;
+	});
+
+	function getCountry(config, callback) {
+		if($routeParams['id']) {
+			$http.get('/api/countries/'+$routeParams['id'], config).then(function(response) {
+				callback(response);
+			});
+		}
+		else {
+			callback({'data':{
+				'name': '', 
+				'slug': '', 
+				'default': true
+			}});
+		}
+	}
+
+	$scope.saveCountry = function(stay) {
+		// If it should UPDATE a product with a PUT Call
+		if($scope.country.id){
+			$http.put('/api/countries/'+$scope.country.id+'/?callback=JSON_CALLBACK', $scope.country).then(function(response){
+				$('#savebox').show().delay(4000).fadeOut();  // Display the "Saved!" box and fade it out after a few seconds.
+
+				// If stay is not true, then navigate the user back to category page.
+				if(!stay){
+					$scope.navigateTo('/localization/');
+				}
+
+			});
+		}
+		// If it should CREATE a product with a POST call
+		else {
+			$http.post('/api/countries/?callback=JSON_CALLBACK', $scope.country).then(function(response){
+				$('#savebox').show().delay(4000).fadeOut();  // Display the "Saved!" box and fade it out after a few seconds.
+
+				// If stay is not true, then navigate the user back to category page.
+				if(!stay){
+					$scope.navigateTo('/localization/');
+				}
+				else {
+					$scope.navigateTo('/localization/country/'+response.data.id);
+				}
+			});
+		}
+	};
+
+	$scope.deleteCountry = function() {
+		$http.delete('/api/countries/'+$scope.country.id+'/?callback=JSON_CALLBACK').then(function(response){
+			$scope.navigateTo('/localization/');
+		});
+	};
+});
--- urls.py
+++ urls.py
@@ -6,7 +6,7 @@ from lindshop.core.cart.views import ajax_cart
 from lindshop.core.checkout.views import checkout, ajax_checkout, process_checkout, thank_you
 from lindshop.core.payment.views import payment_webhook, get_form
 from lindshop.core.subscription.views import subscription_cancel, subscription_change
-from lindshop.core.dashboard.api import UserViewSet, OrderViewSet, CartViewSet, ProductViewSet, CategoryViewSet, PricingViewSet, TaxruleViewSet, CurrencyViewSet, ProductImageViewSet, AttributeViewSet, StockViewSet, WarehouseViewSet, MenuViewSet, SlideshowViewSet, ProductDataPresetViewSet
+from lindshop.core.dashboard.api import UserViewSet, OrderViewSet, CartViewSet, ProductViewSet, CategoryViewSet, PricingViewSet, TaxruleViewSet, CurrencyViewSet, ProductImageViewSet, AttributeViewSet, StockViewSet, WarehouseViewSet, MenuViewSet, SlideshowViewSet, ProductDataPresetViewSet, CountryViewSet
 from rest_framework import routers
 
 app_name = 'lindshop'
@@ -49,6 +49,7 @@ router.register(r'stock', StockViewSet, base_name="Stock")
 router.register(r'menus', MenuViewSet, base_name="Menu")
 router.register(r'slideshows', SlideshowViewSet, base_name="Slideshow")
 router.register(r'productdatapresets', ProductDataPresetViewSet, base_name="DataPreset")
+router.register(r'countries', CountryViewSet, base_name="Country")
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
