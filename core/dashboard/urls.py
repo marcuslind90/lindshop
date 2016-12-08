9ad7117582ef6d569a01bc501ad5aaa3d979efe9
marcuslind90 <marcuslind90@gmail.com>
2016-12-08 18:07:04 +0700

    Removed Deprecated URL patterns for Django 1.10

--- core/dashboard/urls.py
+++ core/dashboard/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url, patterns
+from django.conf.urls import url
 from django.contrib.auth.views import login, logout
 from lindshop.core.dashboard import views
 
--- urls.py
+++ urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from lindshop import views
 from lindshop.core.product.views import product
 from lindshop.core.category.views import category
